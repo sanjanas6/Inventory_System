b21c1e5ca06df382a85712bc6ddff9f47bb6a662
Sanjana <sanjanasingh7774@gmail.com>
2026-05-27 17:43:51 +0530

    Added health route

--- server/index.js
+++ server/index.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const PORT = process.env.PORT || 5000;
 require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-})
-    .then(() => console.log("DB Connected"))
-    .catch(err => console.log("DB Error:", err));
-app.get("/", (req, res) => {
-    res.send("API Running");
-});
-
-app.listen(PORT, () =>
-    console.log(`Server running on ${PORT}`)
-);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.log("DB Error:", err));
 
 const entryRoutes = require("./routes/entryRoutes");
 
 app.use("/api/entries", entryRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("Server alive");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on ${PORT}`)
+);
